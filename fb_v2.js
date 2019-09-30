d1a3f429cba210d9119a7e421b4c09625389fa2b
Aleti Srinivasarao <alti@Administrators-MacBook-Pro-2.local>
2019-09-30 15:03:48 +0530

    Scorll ten by ten

--- fb_v2.js
+++ fb_v2.js
@@ -1,46 +1,40 @@
 const puppeteer = require("puppeteer");
 var fs = require("fs");
 var util = require("util");
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false
-  });
 
+const fetchPosts = async (browser, from, to) => {
+  console.log("Crawling Post " + from + " to Post " + to);
   const page = await browser.newPage();
   await page.viewport({
     width: 1920,
     height: 1080
   });
   await page.goto("https://www.facebook.com/events/");
+  await page.waitFor(2000); //wait for page to load
 
-  await page.waitFor(2000);
-
-  // await autoScroll(page);
-  await scrollTo(page, 100);
-
+  await scrollTo(page, from, to);
   const ul = await page.$$(".uiList");
   const result = [];
-  for (i = 0; i < 100; i++) {
+  for (let i = from; i < to; i++) {
     const posts = await ul[5].$$("li");
     const targetedPost = posts[i];
-    expandPost(page, targetedPost);
     await page.waitFor(2000);
     let dim = await fetchPost(page, targetedPost);
     console.log(
       "============================================================================================================\n"
     );
 
-    console.log(dim.title);
+    console.log({ [i]: dim });
     result.push(dim);
   }
-
-  fs.writeFileSync("./temp.js", util.inspect(result), "utf-8");
-})();
+  await page.close();
+  return result;
+};
 
 const expandPost = async (page, post) => {
   let stop = false;
   while (true) {
-    await page.waitFor(100);
+    await page.waitFor(200);
     if (stop) {
       break;
     }
@@ -62,6 +56,12 @@ const fetchPost = async (page, post) =>
         .querySelector(".clearfix")
         .querySelector(".lfloat")
         .querySelectorAll("span");
+      const img = li
+        .querySelector("div")
+        .querySelector("div")
+        .querySelector("div")
+        .querySelector("a")
+        .querySelector("img").src;
       const title = li.querySelectorAll(".clearfix")[1].querySelector("a").text;
       const place = li.querySelectorAll(".clearfix")[1].querySelector("span")
         .textContent;
@@ -69,6 +69,7 @@ const fetchPost = async (page, post) =>
         description,
         date: dateSpans[1].textContent + " " + dateSpans[2].textContent,
         title,
+        img,
         place
       };
     } catch (e) {
@@ -76,47 +77,63 @@ const fetchPost = async (page, post) =>
     }
   }, post);
 
-async function scrollTo(page, index) {
+async function scrollTo(page, fromIndex, index) {
   for (i = 0; i < index; i++) {
-    let dim = await page.evaluate(
-      async ({ i }) => {
-        const postsUl = document.querySelectorAll(".uiList")[5];
-        li = postsUl.querySelectorAll("li")[i];
-        function getOffset(el) {
-          const rect = el.getBoundingClientRect();
-          return {
-            x: rect.left + window.scrollX,
-            y: rect.top + window.scrollY,
-            height: rect.height,
-            width: rect.width
-          };
-        }
-        return getOffset(li);
-      },
-      { i }
-    );
+    console.log("Scrolling : " + i);
+    if (i >= fromIndex) {
+      const postsContainer = await page.$$(".uiList");
+      const posts = await postsContainer[5].$$("li");
+      await expandPost(page, posts[i]);
+    }
+
+    let dim =
+      (await page.evaluate(
+        async ({ i }) => {
+          try {
+            const postsUl = document.querySelectorAll(".uiList")[5];
+            li = postsUl.querySelectorAll("li")[i];
+            function getOffset(el) {
+              const rect = el.getBoundingClientRect();
+              const { height = 10, width = 10 } = rect;
+              return {
+                x: rect.left + window.scrollX,
+                y: rect.top + window.scrollY,
+                height,
+                width
+              };
+            }
+            return getOffset(li);
+          } catch (e) {
+            console.log("Scrolling Stopped");
+          }
+        },
+        { i }
+      )) || {};
     await page.waitFor(1000);
     await page.evaluate(height => {
       window.scrollBy(0, height);
     }, dim.height);
   }
+  console.log("Scroll Completed");
 }
 
-async function autoScroll(page) {
-  await page.evaluate(async () => {
-    await new Promise((resolve, reject) => {
-      var totalHeight = 0;
-      var distance = 100;
-      var timer = setInterval(() => {
-        var scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-
-        if (totalHeight >= scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 100);
-    });
+const getData = async () => {
+  let result = [];
+  const inc = 10;
+  const browser = await puppeteer.launch({
+    headless: false
   });
-}
+  for (let j = 0; j < 40; j += inc) {
+    try {
+      const partialResult = await fetchPosts(browser, j, j + inc);
+      result = [...result, ...partialResult];
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  await browser.close();
+
+  fs.writeFileSync("./temp.js", util.inspect(result), "utf-8");
+};
+
+getData();
